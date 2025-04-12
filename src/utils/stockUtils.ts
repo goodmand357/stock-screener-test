9165752b19c6a9418e328c9ecafb9a4a1c5fae0c
Danisha <goodmand@montclair.edu>
2025-04-11 22:38:23 -0400

    Create stockUtils.ts

---
+++ src/utils/stockUtils.ts
@@ -0,0 +1,9 @@
+
+export const getPerformanceData = (symbol: string) => [
+  { year: '2019', value: 100 },
+  { year: '2020', value: 120 },
+  { year: '2021', value: 180 },
+  { year: '2022', value: 150 },
+  { year: '2023', value: 200 },
+  { year: '2024', value: 220 },
+];
